25363a229b74a21bda926b00e70b5bff42bb4577
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-07-18 07:16:56 +0300

    After a lot of trials and tribulations

--- test/Coldchain.js
+++ test/Coldchain.js
@@ -22,7 +22,7 @@ contract("ColdChain", (accounts) => {
       PROVER: { val: "PROVER", pos: 2 },
     };
     this.StatusEnums = {
-      manufacured: { val: "MANUFACTURED", pos: 0 },
+      manufactured: { val: "MANUFACTURED", pos: 0 },
       delivering1: { val: "DELIVERING_INTERNATIONAL", pos: 1 },
       stored: { val: "STORED", pos: 2 },
       delivering2: { val: "DELIVERING_LOCAL", pos: 3 },
@@ -88,8 +88,97 @@ contract("ColdChain", (accounts) => {
         entityId: id,
         entityMode: mode,
       });
-      break;
-      // assert.equal(actual, expected, errorMessage);
     }
+  });
+
+
+  it("should add vaccine batches successfully", async () => {
+    for (let i = 0; i < Object.keys(this.defaultVaccineBatches).length; i++) {
+      const { brand, manufacturer } = this.defaultVaccineBatches[i];
+
+      const result = await this.coldChainInstance.addVaccineBatch(
+        brand,
+        manufacturer,
+        {
+          from: this.owner,
+        }
+      );
+      expectEvent(result.receipt, "AddVaccineBatch", {
+        vaccineBatchId: String(i),
+        manufacturer: manufacturer,
+      });
+
+      const retreivedVaccineBatch =
+        await this.coldChainInstance.VaccineBatches.call(i);
+      assert.equal(i, retreivedVaccineBatch.id);
+      assert.equal(brand, retreivedVaccineBatch.brand);
+      assert.equal(manufacturer, retreivedVaccineBatch.manufacturer);
+      assert.equal(undefined, retreivedVaccineBatch.certificateIds);
+    }
+  });
+
+  it("should sign a message and store a certificate from the issuer to the prover", async () => {
+    const mnemonic =
+      "void inflict sail case speak inject lift garden suspect bone cotton blush";
+    const providerOrUrl = "http://localhost:8545";
+    const provider = new HDWalletProvider({
+      mnemonic,
+      providerOrUrl,
+    });
+    this.web3 = new Web3(provider);
+    const { inspector, manufacturerA } = this.defaultEntities;
+    const vaccineBatchId = 0;
+    const message = `Inspector (${inspector.id}) has certified vaccine batch #${vaccineBatchId} for Manufacturer (${manufacturerA.id}).`; //what an actual certifiacetw ould look like
+    const signature = await this.web3.eth.sign(
+      this.web3.utils.keccak256(message),
+      inspector.id
+    );
+
+    const result = await this.coldChainInstance.issueCertificate(
+      inspector.id,
+      manufacturerA.id,
+      this.StatusEnums.manufactured.val,
+      vaccineBatchId,
+      signature,
+      { from: this.owner }
+    );
+
+    expectEvent(result.receipt, "IssueCertificate", {
+      issuer: inspector.id,
+      prover: manufacturerA.id,
+      certificateId: new BN(0),
+    });
+
+    const retreivedCertificate = await this.coldChainInstance.certificates.call(
+      0
+    );
+
+    assert.equal(retreivedCertificate.id, 0);
+    assert.equal(retreivedCertificate.issuer["id"], inspector.id);
+    assert.equal(retreivedCertificate.prover["id"], manufacturerA.id);
+    assert.equal(retreivedCertificate.signature, signature);
+    assert.equal(
+      retreivedCertificate.status,
+      this.StatusEnums.manufactured.pos.toString()
+    );
+  });
+
+  it("should verify that the certificate signature matches the user", async () => {
+    const { inspector, manufacturerA } = this.defaultEntities;
+    const vaccineBatchId = 0;
+    const message = `Inspector (${inspector.id}) has certified vaccine batch #${vaccineBatchId} for Manufacturer (${manufacturerA.id}).`; //what an actual certifiacetw ould look like
+
+    const certificate = await this.coldChainInstance.certificates.call(0);
+
+    const signerMatches = await this.coldChainInstance.isMatchingSignature(
+      this.web3.utils.keccak256(message),
+      certificate.id,
+      inspector.id,
+      {
+        from: this.owner,
+      }
+    );
+
+    assert.equal(signerMatches, true);
   });
 });
